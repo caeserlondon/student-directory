4af9db28c006a8709d9b8fb1c5221fa1bbe05a8f
Caeser F Ibrahim <77758062+caeserlondon@users.noreply.github.com>
2021-10-18 01:32:33 +0100

    using variable insted of a number

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 11
+# first we print the list of students
 puts 'The students of Villains Academy'
 puts '-------------'
 puts 'Dr. Hannibal Lecter'
@@ -11,3 +13,5 @@ puts 'Freddy Krueger'
 puts 'The Joker'
 puts 'Joffrey Baratheon'
 puts 'Norman Bates'
+# finally, we print the total number of students
+puts "Overall, we have #{student_count} great students"
